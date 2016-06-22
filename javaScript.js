badda6d7e25657ea8aae50c8786ee7c3f0bb218f
noonana73 <ppalle73@gmail.com>
2016-06-23 09:31:54 +1000

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,4 @@
 var calc1 = parseInt(prompt("Entrer votre premier nombre a calculer"));
 var calc2 = parseInt(prompt("Entrer votre deuxieme nombre a calculer"));
 var total = calc1 + calc2;
-alert(total);
+alert("Le resulat de "+ calc1 + " + "+ calc2+" = "total);
