d08302a4e08dc96621920404717be8605cf051a1
noonana73 <ppalle73@gmail.com>
2016-06-23 09:28:26 +1000

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,8 +1,6 @@
 var calc1 = q;
 var clac2 = q;
-while(isNaN(clac1)===true && isNaN(clac2)===true){
   calc1 = parseInt(prompt("Entrer votre premier nombre a calculer"));
   calc2 = parseInt(prompt("Entrer votre deuxieme nombre a calculer"));
-}
 var total = calc1 + clac2;
 alert(total);
