574463b93c05494c29b947ef36f9063cb2516319
noonana73 <ppalle73@gmail.com>
2016-06-23 10:05:24 +1000

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,12 @@
-var family = {
-  self: "Sebastien",
-  sister: "ta mere la pute",
-  parent: "Jean de la fontaine",
-};
-alert(family.self);
+var nicks = [], // Création du tableau vide
+    nick;
+
+while (nick = prompt('Entrez un prénom :')) { // Si la valeur assignée à la variable « nick » est valide (différente de « null ») alors la boucle s'exécute
+    nicks.push(nick + " "); // Ajoute le nouveau prénom au tableau
+}
+
+if (nicks.length > 0) { // On regarde le nombre d'items
+    alert(nicks); // Affiche les prénoms à la suite
+} else {
+    alert('Il n\'y a aucun prénom en mémoire !');
+}
