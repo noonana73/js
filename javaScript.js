b5894575a7f8fa8c3217937f62e1b1caa68c8cd0
noonana73 <ppalle73@gmail.com>
2016-06-23 10:00:12 +1000

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,4 +1,6 @@
-var calc1 = parseInt(prompt("Entrer votre premier nombre a calculer"));
-var calc2 = parseInt(prompt("Entrer votre deuxieme nombre a calculer"));
-var total = calc1 + calc2;
-alert("Le resulat de "+ calc1 + " + "+ calc2+" = "+total);
+var family = {
+  self: "Sebastien",
+  sister: "ta mere la pute",
+  parent: "Jean de la fontaine",
+};
+alert(family.self);
